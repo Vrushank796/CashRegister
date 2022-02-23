0b0dad111a4f3ad62023bf0e1034f13c7047a525
Vrushank796 <42446203+Vrushank796@users.noreply.github.com>
2022-02-22 21:25:29 -0500

    Initial Commit

---
+++ CashRegister/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CashRegister
+//
+//  Created by Vrushank on 2022-02-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
